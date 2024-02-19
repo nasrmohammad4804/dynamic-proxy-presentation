86d2fc5a888761501bb254ed985037cb4849bfac
mohammad nasr <nasrmohammad4804@gmail.com>
2024-02-19 04:49:19 -0800

    remove all files

--- dynamic-proxy/src/main/java/org/example/jdk/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.example.jdk;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
--- dynamic-proxy/src/main/java/org/example/jdk/UserService.java
+++
@@ -1,8 +0,0 @@
-package org.example.jdk;
-
-public interface UserService {
-
-    void addUser(User user) throws Throwable;
-
-    User getById(Long id) throws Throwable;
-}
